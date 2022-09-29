433774bef8caf3be00d23eab7b498765fbe6196e
Albert de Paula <albert_depaula@yahoo.com.br>
2022-09-29 11:06:35 -0300

    Alterada porta padrão para a do heroku

--- src/server.ts
+++ src/server.ts
@@ -25,4 +25,4 @@ app.use((err: Error, request: Request, response:Response, next: NextFunction) =>
     })
 })
 
-app.listen(3333, ()=> console.log("Server is running in port: 3333"));
+app.listen(process.env.PORT || 3000);
